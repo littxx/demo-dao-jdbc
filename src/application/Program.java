ba4cc25c2d7319c3e55e7e622297d7c940f71329
EdenMaisUmDia <ederlanbrito@gmail.com>
2022-09-10 14:34:15 -0300

    mader

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Departament;
 import model.entities.Seller;
 
@@ -16,6 +18,9 @@ public class Program {
 	
 		
 		Seller vender = new Seller("eden", 1, "eden", new Date(), 50.5, loko);
+		
+		SellerDao vendedor = DaoFactory.createSellerDao();
+		
 		System.out.println(vender);
 		
 		

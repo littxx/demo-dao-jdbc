b97e8dd9b94121eaf367fd723b2e31f2aa752ef9
EdenMaisUmDia <ederlanbrito@gmail.com>
2022-09-10 11:09:04 -0300

    ok

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Departament;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		Departament loko = new Departament("books", 1);
+		System.out.println(loko);
+	}
+
+}
---
+++ src/model/entities/Departament.java
@@ -0,0 +1,69 @@
+package model.entities;
+
+import java.io.Serializable;
+
+public class Departament implements Serializable{
+
+	
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private Integer id; 
+	
+	public Departament() {
+		
+	}
+
+	public Departament(String name, Integer id) {
+		this.name = name;
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Departament other = (Departament) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "departament [name=" + name + ", id=" + id + "]";
+	}
+	
+	
+	
+}
